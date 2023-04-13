5fcd19456b10cdd85fe1c0bd3fdf511a5ba288d8
Daniela V <liliangarcia@Lilians-MacBook-Air.local>
2023-04-12 20:31:03 -0400

    updated cart page

--- pages/cart.js
+++ pages/cart.js
@@ -1,8 +1,22 @@
 import Layout from '@/components/layout';
 import { Store } from '@/utils/Store';
-import { TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import {
+  ListItem,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
 import React, { useContext } from 'react';
-import NextLink from 'next/link ';
+import NextLink from 'next/link';
+import Grid from '@mui/material/Grid';
+import Table from '@mui/material/Table';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
+import Image from 'next/image';
+import Select from '@mui/material';
+import MenuItem from '@mui/material';
+import Button from '@mui/material';
 
 export default function CartView() {
   const { state } = useContext(Store);
@@ -11,7 +25,7 @@ export default function CartView() {
   } = state;
   return (
     <Layout title="Review Shopping Cart">
-      <Typography component="h1 ">Cart: Checkout</Typography>
+      <Typography component="h1">Cart: Checkout</Typography>
       {cartItems.length === 0 ? (
         <div>
           Your cart is empty! <NextLink href="/">Lets go shopping</NextLink>
@@ -27,15 +41,59 @@ export default function CartView() {
                     <TableCell>Name</TableCell>
                     <TableCell align="right">Price</TableCell>
                     <TableCell align="right">quantity</TableCell>
-                    <TableCell align="right">Button</TableCell>
+                    <TableCell align="right">Delete</TableCell>
                   </TableRow>
                 </TableHead>
+                <TableBody>
+                  {cartItems.map((item) => (
+                    <TableRow key={item.id}>
+                      <TableCell>
+                        <NextLink href={`product/${item.slug}`}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                          ></Image>
+                        </NextLink>
+                      </TableCell>
+
+                      <TableCell>
+                        <NextLink href={`product/${item.slug}`}>
+                          <Typography>{item.name}</Typography>
+                        </NextLink>
+                      </TableCell>
+
+                      <TableCell>
+                        <Select align="right" value={item.quantity}>
+                          {[...Array(item.countInStock).keys()].map((i) => (
+                            <MenuItem key={i + 1} value={i + 1}>
+                              {i + 1}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </TableCell>
+
+                      <TableCell align="right">${item.price}</TableCell>
+
+                      <TableCell>
+                        <Button align="right" color="secondary">
+                          x
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
               </Table>
             </TableContainer>
             Cart Items
           </Grid>
           <Grid md={3} xs={12}>
-            Cart buttons
+            <Card>
+              <List>
+                <ListItem></ListItem>
+              </List>
+            </Card>
           </Grid>
         </Grid>
       )}
