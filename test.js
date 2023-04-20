016e4fa240f2bbbd0b9ff672eaa51da9d92cc977
Daniela V <liliangarcia@Lilians-MacBook-Air.local>
2023-04-20 19:13:54 -0400

    test

--- test.js
+++ test.js
@@ -9,3 +9,15 @@
 // const data = await res.json();
 // res.send(products);
 // res.status(200).json(products);
+
+// await db.connect();
+// const products = await Product.find();
+// const res = await fetch(products);
+// res.status(200).send(products);
+
+// res.status(200).json(products);
+// await db.disconnect();
+// const res = await fetch(products);
+// const data = await res.json();
+// res.send(products);
+// res.status(200).json(products);
